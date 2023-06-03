c1e52c2dae2455c89e415f1896d0b981415ba761
MiguelProgramacionIII <miguel.q.199835@gmail.com>
2023-06-03 14:56:31 -0600

    primer commit

---
+++ src/test/java/com/curso2/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso2.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
